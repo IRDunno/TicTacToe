cb4d048d8a18a9bbe1d9b09cd425f8534eb4ed80
aby calago <calagoaby@gmail.com>
2024-06-29 15:19:32 +0800

    add: added about page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import React from "react";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
