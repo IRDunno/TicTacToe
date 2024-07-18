56f59c0d5a2c7094eb563d40188516037ca7c057
aby calago <calagoaby@gmail.com>
2024-07-18 13:37:45 +0800

    fix: make a card component within HomePage jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,32 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const Card = ({ title, description, link, linkText }) => {
+  return (
+    <div className="card bg-base-100 image-full shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <Link to={link} className="btn btn-muted">
+            {linkText}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const HomePage = () => {
   return (
     <div className="mt-20">
       <header className="text-center space-y-3">
         <h1 className="text-4xl font-bold">Welcome to TicTacToe</h1>
-        <p className="">Choose gamemode</p>
+        <p>Choose gamemode</p>
       </header>
       <div className="mt-14 grid grid-cols-1 md:grid-cols-2 gap-4 px-20">
-        <div className="card bg-base-100 image-full shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Duo</h2>
-            <p>Play with your friend on the same computer</p>
-            <div className="card-actions justify-end">
-              <Link to="/play-duo" className="btn btn-muted">Play Now</Link>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-base-100 image-full shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Online</h2>
-            <p>Play online against other players</p>
-            <div className="card-actions justify-end">
-              <Link to="/play-multiplayer" className="btn btn-muted">Play Now</Link>
-            </div>
-          </div>
-        </div>
+        <Card
+          title="Duo"
+          description="Play with your friend on the same computer"
+          link="/play-duo"
+          linkText="Play Now"
+        />
+        <Card
+          title="Online"
+          description="Play online against other players"
+          link="/play-multiplayer"
+          linkText="Play Now"
+        />
       </div>
     </div>
   );
