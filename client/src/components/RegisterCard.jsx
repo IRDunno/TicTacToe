4f7b9a49adedc7506305eb3debdcf0ce628f742c
aby calago <calagoaby@gmail.com>
2024-07-19 17:35:05 +0800

    feat: add registe page

---
+++ client/src/components/RegisterCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const RegisterCard = () => {
+  return (
+    <div className="bg-base-100 my-28 mx-10 p-14 shadow-xl rounded-xl lg:mx-0 md:mx-28">
+      <h1 className="text-3xl font-medium text-center">REGISTER</h1>
+      <div className="grid grid-rows-2 gap-2 mt-10">
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Username:</span>
+          </div>
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </label>
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Password:</span>
+          </div>
+          <input
+            type="password"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+          />
+        </label>
+      </div>
+      <div className="grid grid-rows-1 mt-3">
+        <button className="btn btn-active">Register</button>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterCard
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import RegisterCard from "../components/RegisterCard";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="grid grid-cols-1 lg:grid-cols-3">
+      <div></div>
+      <RegisterCard />
+      <div></div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
