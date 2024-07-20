d5bd08f2918052a601d4971a15864ef1bdcea14e
aby calago <calagoaby@gmail.com>
2024-07-20 10:49:45 +0800

    fix: add link to login and reg page and vice versa

--- client/src/components/LoginCard.jsx
+++ client/src/components/LoginCard.jsx
@@ -1,44 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const LoginCard = () => {
   return (
-    <div className="bg-base-100 my-28 mx-10 p-14 shadow-xl rounded-xl lg:mx-0 md:mx-28 w-[500px]">
-      <h1 className="text-3xl font-medium text-center">SIGN IN</h1>
-      <div className="grid grid-rows-2 gap-2 mt-10">
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text">Username:</span>
-          </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text">Password:</span>
-          </div>
-          <input
-            type="password"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
-        </label>
-      </div>
-      <div className="grid grid-rows-2 gap-2 mt-3">
-        <div className="flex justify-between items-center">
-          <div className="form-control">
-            <label className="label cursor-pointer gap-2 items-center">
-              <input type="checkbox" className="checkbox checkbox-xs" />
-              <span className="label-text">Remember me</span>
-            </label>
-          </div>
-          <div>
-            <span className="label-text hover:underline hover:pointer hover:cursor-pointer">Forgot Password</span>
-          </div>
+    <div className="my-28 mx-10 lg:mx-0 md:mx-28 w-[500px]">
+      <div className="bg-base-100 p-14 shadow-xl rounded-xl">
+        <h1 className="text-3xl font-medium text-center">SIGN IN</h1>
+        <div className="grid grid-rows-2 gap-2 mt-10">
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Username:</span>
+            </div>
+            <input
+              type="text"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Password:</span>
+            </div>
+            <input
+              type="password"
+              className="input input-bordered w-full"
+            />
+          </label>
         </div>
-        <button className="btn btn-active">Sign in</button>
+        <div className="grid grid-rows-2 gap-2 mt-3">
+          <div className="flex justify-between items-center">
+            <div className="form-control">
+              <label className="label cursor-pointer gap-2 items-center">
+                <input type="checkbox" className="checkbox checkbox-xs" />
+                <span className="label-text">Remember me</span>
+              </label>
+            </div>
+            <div>
+              <span className="label-text hover:underline hover:pointer hover:cursor-pointer">
+                Forgot Password
+              </span>
+            </div>
+          </div>
+          <button className="btn btn-active">Sign in</button>
+        </div>
+      </div>
+      <div className="text-end pr-1">
+        <Link
+          to="/register"
+          className="label-text cursor-pointer hover:underline"
+        >
+          No account? Register now!
+        </Link>
       </div>
     </div>
   );
--- client/src/components/RegisterCard.jsx
+++ client/src/components/RegisterCard.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const RegisterCard = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerForm = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      username,
+      password,
+    };
+  };
+
   return (
-    <div className="bg-base-100 my-28 mx-10 p-14 shadow-xl rounded-xl lg:mx-0 md:mx-28 w-[500px]">
-      <h1 className="text-3xl font-medium text-center">REGISTER</h1>
-      <div className="grid grid-rows-2 gap-2 mt-10">
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text">Username:</span>
-          </div>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text">Password:</span>
-          </div>
-          <input
-            type="password"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
-        </label>
-      </div>
-      <div className="grid grid-rows-1 mt-3">
-        <button className="btn btn-active">Register</button>
+    <div className="my-28 mx-10 lg:mx-0 md:mx-28 w-[500px]">
+      <form
+        className="bg-base-100 p-14 shadow-xl rounded-xl"
+        onSubmit={registerForm}
+      >
+        <h1 className="text-3xl font-medium text-center">REGISTER</h1>
+        <div className="grid grid-rows-2 gap-2 mt-10">
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Username:</span>
+            </div>
+            <input
+              type="text"
+              value={username}
+              className="input input-bordered w-full"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Password:</span>
+            </div>
+            <input
+              type="password"
+              value={password}
+              className="input input-bordered w-full"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="grid grid-rows-1 mt-3">
+          <button className="btn btn-active">Register</button>
+        </div>
+      </form>
+      <div className="text-end pr-1">
+        <Link
+          to="/login"
+          className="label-text cursor-pointer hover:underline"
+        >
+          Alreay has an account? Sign in!
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterCard
+export default RegisterCard;
