e51939867a90f27de00505787bb21feb1fa148f7
aby calago <calagoaby@gmail.com>
2024-07-30 10:02:49 +0800

    jwt authentication by dave gray

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,6 +8,8 @@ const usersDB = {
 const fsPromises = require("fs").promises;
 const path = require("path");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const register = async (req, res) => {
   const { username, password } = req.body;
@@ -25,7 +27,6 @@ const register = async (req, res) => {
       path.join(__dirname, "..", "models", "users.json"),
       JSON.stringify(usersDB.users)
     );
-    console.log(usersDB.users);
     res
       .status(201)
       .json({ success: true, message: "User created", data: usersDB.users });
@@ -51,7 +52,81 @@ const login = async (req, res) => {
     return res
       .status(401)
       .json({ success: false, message: "Invalid username or password" });
-  res.status(200).json({ success: true, message: "User logged in" });
+
+  const accessToken = jwt.sign(
+    { username: foundUser.username },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "30s" }
+  );
+  const refreshToken = jwt.sign(
+    { username: foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+  const otherUsers = usersDB.users.filter(
+    (user) => user.username !== foundUser.username
+  );
+  const currentUser = { ...foundUser, refreshToken };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "models", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  res
+    .status(200)
+    .json({ success: true, message: "User logged in", accessToken });
 };
 
-module.exports = { register, login };
+const refreshToken = (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401);
+  const refreshToken = cookies.jwt;
+
+  const foundUser = usersDB.users.find(
+    (user) => user.refreshToken === refreshToken
+  );
+  if (!foundUser) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username)
+      return res.sendStatus(403);
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
+    res.json({ accessToken });
+  });
+};
+
+const logout = async (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204);
+  const refreshToken = cookies.jwt;
+
+  const foundUser = usersDB.users.find(
+    (user) => user.refreshToken === refreshToken
+  );
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true });
+    return res.sendStatus(204);
+  }
+
+  const otherUsers = usersDB.users.filter(
+    (user) => (user.f = refreshToken !== foundUser.refreshToken)
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "models", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true });
+  res.sendStatus(204);
+};
+
+module.exports = { register, login, refreshToken, logout };
